20e542fddbd42122d9d604a5b47e9a5164da8622
yumi.funa <yumiko.2133.funa@gmail.com>
2021-10-12 23:01:28 +0900

    first-commit

---
+++ js/common.js
@@ -0,0 +1,27 @@
+window.onunload = function() {};
+
+function sizecheck() {
+    w = window.innerWidth ? window.innerWidth : $(window).width();
+    h = window.innerHeight ? window.innerHeight : $(window).height();
+}
+
+sizecheck();
+
+$(function () {
+
+    if(w > 769) {
+} else {
+$("header .drawer").click(function(){
+$('body').toggleClass('nav-open');
+$('header .navbar').fadeToggle(200);
+});
+
+$("header li a").click(function(){
+$('body').toggleClass('nav-open');
+$('header .navber').fadeToggle(200);
+});
+
+} 
+
+});
+
